e56250eebdb96a316abd455bd3c863428e25b9f9
ervi <ervilidyawati@student.uir.ac.id>
2024-12-20 18:50:58 +0700

    Pertemuan6

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors'); 
+const authRoutes = require('./routes/authRoutes');
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+dotenv.config();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const app = express();
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || 'localhost';
+
+app.use(cors()); 
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/api/auth', authRoutes);
+
+app.get("/", (req, res) => {
+  res.send({ status: "mulai" })
+})
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
